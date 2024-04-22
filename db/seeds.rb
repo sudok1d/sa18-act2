00941c4d76409cba819bb2b603b40ee4944b225c
Asiah Clark <clarkaa0@gmail.com>
2024-04-22 17:42:30 -0500

    Implemented Product model, controller, views, and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Product.create!(name: "Apple", price: 3.99, description: "Delicious crisp gala apple")
+Product.create!(name: "Mechanical Keyboard", price: 110.99, description: "Clicky tactile custom keyboard with cherry red switches")
+Product.create!(name: "Wireless mouse", price: 15.49, description: "SMooth clicky mouse with 3000 dpi")
